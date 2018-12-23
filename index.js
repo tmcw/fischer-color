fd0f92c40c9f4c350d50b0b7354d9ab23387e709
Tom MacWright <tom@macwright.org>
2018-12-23 15:37:31 -0800

    refactor: Switch to microbundle and ES modules
    BREAKING CHANGE: this adds module, unpkg, and umd:main entries.

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
-module.exports.interpolate =
-
 /**
  * @param h hue in range of [0..2π]
  * @param c chroma in [0..1]
  * @param l luminosity in range of [0..1]
  * @returns Array of [r, g, b] values.
  */
-function interpolate(h, c, l) {
-    'use strict';
+export function interpolate(h, c, l) {
+  // unpack for the lazy
+  if (Array.isArray(h)) {
+    [h, c, l] = h;
+  }
 
-    // unpack for the lazy
-    if (Array.isArray(h)) {
-        c = h[1]; l = h[2]; h = h[0];
-    }
+  // put red at the right
+  h = h + (Math.PI / 2 - (Math.PI - 2));
 
-    // put red at the right
-    h = h + (Math.PI / 2 - (Math.PI - 2));
+  // keep chroma in range for this lightness
+  if (c > 1 - 2 * Math.abs(l - 0.5)) {
+    c = 1 - 2 * Math.abs(l - 0.5);
+  }
 
-    // keep chroma in range for this lightness
-    if (c > 1 - 2 * Math.abs(l - 0.5)) {
-        c = 1 - 2 * Math.abs(l - 0.5);
-    }
+  const r1 = Math.sin(h + Math.PI - 2.0) * 0.417211 * c + l,
+    g1 = Math.sin(h + Math.PI + 1.5) * 0.158136 * c + l,
+    b1 = Math.sin(h + Math.PI) * 0.455928 * c + l;
 
-    var r1 = Math.sin(h + Math.PI - 2.0) * 0.417211 * c + l,
-        g1 = Math.sin(h + Math.PI + 1.5) * 0.158136 * c + l,
-        b1 = Math.sin(h + Math.PI      ) * 0.455928 * c + l;
-
-    return [
-        // red
-        Math.exp(Math.log(r1 * 0.923166 + 0.0791025) * 1.25) * 255,
-        // green
-        Math.exp(Math.log(g1 * 0.923166 + 0.0791025) * 1.25) * 255,
-        // blue
-        Math.exp(Math.log(b1 * 0.923166 + 0.0791025) * 1.25) * 255];
-};
+  return [
+    // red
+    Math.exp(Math.log(r1 * 0.923166 + 0.0791025) * 1.25) * 255,
+    // green
+    Math.exp(Math.log(g1 * 0.923166 + 0.0791025) * 1.25) * 255,
+    // blue
+    Math.exp(Math.log(b1 * 0.923166 + 0.0791025) * 1.25) * 255
+  ];
+}
--- test.js
+++ test.js
@@ -1,10 +1,21 @@
-var test = require('tape'),
-    interpolate = require('./').interpolate;
+var test = require("tape"),
+  interpolate = require("./").interpolate;
 
-
-test('interpolate', function(t) {
-    t.deepEqual(interpolate(0, 0, 0), [ 10.697392778616015, 10.697392778616015, 10.697392778616015 ]);
-    t.deepEqual(interpolate(0.1, 0.1, 0.1), [ 36.1949026281989, 25.467500151348798, 23.838016883988544 ]);
-    t.deepEqual(interpolate([0.1, 0.1, 0.1]), [ 36.1949026281989, 25.467500151348798, 23.838016883988544 ]);
-    t.end();
+test("interpolate", function(t) {
+  t.deepEqual(interpolate(0, 0, 0).map(n => +n.toFixed(5)), [
+    10.69739,
+    10.69739,
+    10.69739
+  ]);
+  t.deepEqual(interpolate(0.1, 0.1, 0.1).map(n => +n.toFixed(5)), [
+    36.1949,
+    25.4675,
+    23.83802
+  ]);
+  t.deepEqual(interpolate([0.1, 0.1, 0.1]).map(n => +n.toFixed(5)), [
+    36.1949,
+    25.4675,
+    23.83802
+  ]);
+  t.end();
 });
